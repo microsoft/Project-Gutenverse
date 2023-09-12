54447c85f0df65cc354edc9338c5bf4851639cc8
Colter Norick <cnorick@microsoft.com>
2023-09-12 21:08:25 +0000

    Merged PR 3458: Removed run.py for using flask run command
    Removed run.py for using flask run command
    Added fleshed post api out to include file management
    fixed environment variable config loading
    refactored scene to a dataclass

--- server/app.py
+++ server/app.py
@@ -1,10 +1,11 @@
 import os
 import json
-import uuid
 from flask import Flask, request, jsonify
 from config import config
 from pipeline import Pipeline
-from pipelinecontext import PipelineContext
+from pipeline import PipelineContext
+from db import DB
+
 app = Flask(__name__)
 
 @app.route("/stories/<story_id>", methods=["GET"])
@@ -62,29 +63,37 @@ def get_stories():
 def create_story():
     data = request.get_json()
     if not data or 'title' not in data or not data['title'] or 'body' not in data or not data['body']:
-        return jsonify({"error": "Invalid payload. 'title' and 'body' fields are required and should be non-empty."}), 400
+        return {"error": "Invalid payload. 'title' and 'body' fields are required and should be non-empty."}, 400
 
     context = process_story(data['title'], data['body'])
-    return jsonify({"guid": context.id}), 201
+    return {"guid": context.id}, 201
 
 @app.route("/upload_story", methods=["POST"])
 def upload_story():
     title = request.form.get('title')
     uploaded_file = request.files.get('file')
     if not title:
-        return jsonify({"error": "The 'title' field is required."}), 400
+        return {"error": "The 'title' field is required."}, 400
     if not uploaded_file:
-        return jsonify({"error": "A file must be uploaded."}), 400
+        return {"error": "A file must be uploaded."}, 400
 
     story_data = uploaded_file.read().decode('utf-8')
     context = process_story(title, story_data)
-    return jsonify({"guid": context.id}), 201
+    return {"guid": context.id}, 201
 
 def process_story(title, story_data):
-    context = PipelineContext()
-    context.title = title
-    context.story_data = story_data
-    
+    context = PipelineContext(title=title, story_data=story_data)    
     pipeline = Pipeline()
     pipeline.execute(context)
     return context
+
+# Create the stories directory if it doesn't exist
+if not os.path.exists(config.stories_dir):
+    os.makedirs(config.stories_dir)
+    
+# Provide the mongodb atlas url to connect python to mongodb using pymongo
+# Create a connection using MongoClient
+DB.connect(config.MongoDBConnectionString)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- server/pipeline/analyzerstage.py
+++ server/pipeline/analyzerstage.py
@@ -4,30 +4,17 @@ import os
 import json
 from stage import Stage
 from config import config
+from llm import llm
 
 class AnalyzerStage(Stage):
     def process(self, context):
-        dotenv.load_dotenv()
-        openai.api_key = os.getenv ("OpenAIApiKey")
-
         for i, scene in enumerate(context.segmentation_analysis.scenes):
-            output = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo-16k",
-                messages=[{
-                    "role": "user",
-                    "content": self.create_analysis_prompt(scene["body"])
-                }]
-            )
-
-            print(output)
+            analyis = llm.analize_scene(scene)
             try:
-                self.save_response_output(output["choices"][0]["message"]["content"], context, i)
+                self.save_response_output(analyis, context, i)
                 print("Successfully produced analysis for scene " + str(i))
             except:
                 print("Encountered an error processing story " + str(i) + " skipping this story")
-                
-
-        return output
     
     def save_response_output(self, response_payload, context, sceneIndex):
         filename = "1_analysis_stage.json"
@@ -40,12 +27,3 @@ class AnalyzerStage(Stage):
         with open(file, "w") as f:
             json.dump(json_data, f, indent=4)
 
-    def create_analysis_prompt(self, story_data):
-        file_path = os.path.join(".\server\pipeline\prompts", "analyzer_stage_prompt.txt")
-        
-        with open(file_path, 'r') as file:
-            content = file.read()
-        
-        analyzer_prompt = content.replace("{{story_data}}", story_data)
-        
-        return analyzer_prompt
