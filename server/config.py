647c311100a2dc7e9ffa3fa3302441ce4a01e148
Jonathan Montemayor <jmontemayor@microsoft.com>
2023-09-12 20:41:13 +0000

    Merged PR 3516: Add Preliminary DB Setup
    Hook up MongoDB with server. Create methods for making new stories and getting all stories. Nothing else besides DB setup so far. Since we're using MongoDB, the data format is super flexible and able to be easily changed.
    
    To use the MongoDB test database, create a MongoDBConnectionString environment variable.

--- server/config.py
+++ server/config.py
@@ -2,22 +2,21 @@ import yaml
 import os
 from dataclasses import dataclass
 from pathlib import Path
-
-# Secret management will be done through enviroment variables for now
-# All non secret config items will be put in a config yaml file    
+# Secret management will be done through environment variables for now
+# All non secret config items will be put in a config yaml file
 
 # Ensure that paths are relative to the script's directory
 _BASE_DIR = Path(__file__).parent
-_CONFIG_FILE = _BASE_DIR / 'config.yaml'
-
-_Secret_Identifier = 'EnviromentVariables'
+_CONFIG_FILE = Path('server/config.yaml')
+_Secret_Identifier = 'EnvironmentVariables'
 
 @dataclass
 class Config:
     server_root: Path
     stories_dir: Path
     OpenAIApiKey: str
-
+    MongoDBConnectionString: str
+    
 def get_secret(name):
     return os.getenv(name)
 
@@ -26,7 +25,7 @@ def _load_config():
     
     with open(_CONFIG_FILE, 'r') as f:
         config = yaml.safe_load(f)
-        config_with_secrets = {k: v if v is not _Secret_Identifier else get_secret(k) for k, v in config.items()}
+        config_with_secrets = {k: v if v != _Secret_Identifier else get_secret(k) for k, v in config.items()}
     
     config_with_secrets['server_root'] = _BASE_DIR
     return config_with_secrets
