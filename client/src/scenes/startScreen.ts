028f55ebd80dd1d78b0eaa529f3c11218854bf48
Ben Lifson <belifson@microsoft.com>
2023-09-13 14:11:39 -0700

    Delete commented out code

--- client/src/scenes/startScreen.ts
+++ client/src/scenes/startScreen.ts
@@ -7,6 +7,7 @@ import { Control } from "@babylonjs/gui/2D/controls/control";
 import {Button } from "@babylonjs/gui/2D/controls/button";
 import { InputTextArea } from "@babylonjs/gui/2D/controls/inputTextArea"
 import { Grid } from "@babylonjs/gui/2D/controls/grid"
+import {StackPanel } from "@babylonjs/gui/2D/controls/stackPanel"
 
 import menuBackgroundUrl from "../../public/assets/menu_background.jpg";
 
@@ -96,6 +97,19 @@ export class StartScreen implements SceneClass {
             const button_closeMenuChooseStory = gui.getControlByName("Button_Close_ChooseStory")!;
             button_closeMenuChooseStory.onPointerUpObservable.add(openMainMenu);
 
+            const stackButtonContainer = gui.getControlByName("StackPanel_StoryButtons")! as StackPanel;
+
+            // TODO: Replace using real data
+            for (var i = 0; i < 8; i++) {
+                var button = Button.CreateSimpleButton("Title" + i, "Title: " + i);
+                button.width = "95.00%";
+                button.height = "60.00px";
+                button.color = "white";
+                button.background = "black";
+                stackButtonContainer.addControl(button);
+            }
+
+
             // === CREATE STORY MENU ===
 
             // Setup the buttons in the Create Story menu
@@ -111,68 +125,6 @@ export class StartScreen implements SceneClass {
             inputTextContainer.addControl(inputTextArea, 1, 0);
             
         });
-
-
-        /*
-        const mainMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
-        let loadedGUI = await  mainMenu.parseFromSnippetAsync("#HHCQ02#36");
-        
-        this.gui = mainMenu;
-
-        // Adding the background image to the mainMenu
-        const backgroundImage = new Image("backgroundImage", menuBackgroundUrl);
-        backgroundImage.width = 1;  // Relative width, covering the full width of the GUI
-        backgroundImage.height = 1; // Relative height, covering the full height of the GUI
-        backgroundImage.stretch = Image.STRETCH_FILL; // Stretch the image to fill the entire GUI
-        mainMenu.addControl(backgroundImage); // Ensure this is the first control added, so it's in the background
-
-        const buttonWidth = "150px";
-        const buttonHeight = "40px";
-        
-        // Header - Gutenverse
-        const header = new TextBlock();
-        header.text = "Gutenverse";
-        header.height = "80px";
-        header.color = "white";
-        header.textHorizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
-        header.fontSize = "48px";
-        header.top = "-150px"
-        mainMenu.addControl(header); 
-
-        // Function to create a standardized button
-        const createButton = function(name: string, text: string, topOffset: string, callback: () => void) {
-            const button = Button.CreateSimpleButton(name, text);
-            button.width = buttonWidth;
-            button.height = buttonHeight;
-            button.color = "black";
-            button.cornerRadius = 5;
-            button.background = "lightgray"; // Set the background color to light gray
-            button.top = topOffset;
-            button.onPointerUpObservable.add(callback);
-            mainMenu.addControl(button);
-            return button;
-        }
-
-        // Choose Story Button
-        createButton("chooseStory", "Choose Story", "-100px", () => {
-            this.startScreenArgs.chooseStoryCallback(); 
-        });
-
-        // Create Story Button
-        createButton("createStory", "Create Story", "-50px", function() {
-            console.log("Create Story clicked");
-        });
-
-        // Settings Button (Stub for now)
-        createButton("settings", "Settings", "0px", function() {
-            console.log("Settings clicked");
-        });
-
-        // Exit Button
-        createButton("exit", "Exit", "50px", function() {
-            window.close();
-        });
-        */
 
         return scene;
     };
