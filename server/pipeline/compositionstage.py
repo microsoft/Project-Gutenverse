f0b56af80ebb626d3bf4a3c784333280124dc1b3
sunnyzms <yayzhang@microsoft.com>
2023-09-13 13:23:19 -0700

    Nit fix.

--- server/pipeline/compositionstage.py
+++ server/pipeline/compositionstage.py
@@ -23,46 +23,45 @@ class CompositionStage(Stage):
             if os.path.isdir(subfolder_path) and '1_analysis_stage.json' in os.listdir(subfolder_path):
                 
                 scene_json_path = os.path.join(subfolder_path, 'scene.json')
+                analysis_stage_json_path = os.path.join(subfolder_path, '1_analysis_stage.json')
+                
                 # Read the JSON file
-                with open(scene_json_path, 'r') as scene_file:
+                with open(scene_json_path, 'r') as scene_file, open(analysis_stage_json_path, 'r') as analysis_file:
                     scene_data = json.load(scene_file)
                     storycontent = scene_data.get('content', '')
                 
-                    analysis_stage_json_path = os.path.join(subfolder_path, '1_analysis_stage.json')
-                    # Read the JSON file
-                    with open(analysis_stage_json_path, 'r') as file:
-                        analysis_data = json.load(file)
-                        characters = analysis_data.get('characters', {})
-                        
-                        # Test data
-                        # story_content = "WOLF, meeting with a Lamb astray from the fold, resolved not to lay violent hands on him, but to find some plea to justify to the Lamb the Wolf’s right to eat him. He thus addressed him: “Sirrah, last year you grossly insulted me.” “Indeed,” bleated the Lamb in a mournful tone of voice, “I was not then born.”"
-                        # characters = "a wolf and a lamb"
+                    analysis_data = json.load(analysis_file)
+                    characters = analysis_data.get('characters', {})
+                    
+                    # Test data
+                    # story_content = "WOLF, meeting with a Lamb astray from the fold, resolved not to lay violent hands on him, but to find some plea to justify to the Lamb the Wolf’s right to eat him. He thus addressed him: “Sirrah, last year you grossly insulted me.” “Indeed,” bleated the Lamb in a mournful tone of voice, “I was not then born.”"
+                    # characters = "a wolf and a lamb"
 
-                        llmOutput = llm.get_composition(story_content=storycontent, characters=json.dumps(characters))
-                        character_composition_data = json.loads(llmOutput)
+                    llmOutput = llm.get_composition(story_content=storycontent, characters=json.dumps(characters))
+                    character_composition_data = json.loads(llmOutput)
 
-                        save_file_path = os.path.join(subfolder_path, '3_composition_stage.json')
-                        if os.path.isfile(save_file_path):
-                            os.remove(save_file_path)
+                    save_file_path = os.path.join(subfolder_path, '3_composition_stage.json')
+                    if os.path.isfile(save_file_path):
+                        os.remove(save_file_path)
 
-                        # Save the aggregated data to a new JSON file in the current subfolder
-                        with open(save_file_path, 'w') as output_file:
-                            json.dump(character_composition_data, output_file, indent=4)
+                    # Save the aggregated data to a new JSON file in the current subfolder
+                    with open(save_file_path, 'w') as output_file:
+                        json.dump(character_composition_data, output_file, indent=4)
 
-                        #############################
-                        
-                        # Hard coded version:
-                        # character_composition_data = {"characters": {}}
-                        # # For each character in the JSON
-                        # for character_name, description in characters.items():
-                        #     character_composition_data['characters'][character_name] = {
-                        #         "distance_from_camera": 1,
-                        #         "distance_from_the_center_in_degree": 0,
-                        #         "distance_from_the_floor": 0,
-                        #         "scale_x": 1,
-                        #         "scale_y": 1,
-                        #     }
-                        # with open(os.path.join(subfolder_path, '3_test_composition_stage.json'), 'w') as output_file:
-                        #     json.dump(character_composition_data, output_file, indent=4)
+                    #############################
+                    
+                    # Hard coded version:
+                    # character_composition_data = {"characters": {}}
+                    # # For each character in the JSON
+                    # for character_name, description in characters.items():
+                    #     character_composition_data['characters'][character_name] = {
+                    #         "distance_from_camera": 1,
+                    #         "distance_from_the_center_in_degree": 0,
+                    #         "distance_from_the_floor": 0,
+                    #         "scale_x": 1,
+                    #         "scale_y": 1,
+                    #     }
+                    # with open(os.path.join(subfolder_path, '3_test_composition_stage.json'), 'w') as output_file:
+                    #     json.dump(character_composition_data, output_file, indent=4)
 
-            return context
+        return context
