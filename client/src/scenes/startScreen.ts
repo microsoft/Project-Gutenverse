5d6ef53a5801269e1a0f581d322fcf0db80c364f
Ben Lifson <belifson@microsoft.com>
2023-09-13 13:40:14 -0700

    Allow for menu switching

--- client/src/scenes/startScreen.ts
+++ client/src/scenes/startScreen.ts
@@ -5,6 +5,8 @@ import { Image } from "@babylonjs/gui/2D/controls/image";
 import { TextBlock } from "@babylonjs/gui/2D/controls/textBlock";
 import { Control } from "@babylonjs/gui/2D/controls/control";
 import {Button } from "@babylonjs/gui/2D/controls/button";
+import { InputTextArea } from "@babylonjs/gui/2D/controls/inputTextArea"
+import { Grid } from "@babylonjs/gui/2D/controls/grid"
 
 import menuBackgroundUrl from "../../public/assets/menu_background.jpg";
 
@@ -13,16 +15,102 @@ export interface StartScreenArgs {
 }
 
 export class StartScreen implements SceneClass {
+
     public gui?: AdvancedDynamicTexture;
     constructor(public startScreenArgs: StartScreenArgs) {}
     populate = async (
         sceneArgs: SceneArgs
     ): Promise<Scene> => {
         const scene = sceneArgs.scene;
+        const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
+
+        // TODO: Add background image
+
+        await AdvancedDynamicTexture.ParseFromSnippetAsync("#HHCQ02#38").then((gui) => {
+            this.gui = advancedTexture;
+
+            // SECTION 1: MAIN MENU
+            // Get all of the menus and sub-menus
+            const mainMenu = gui.getControlByName("MainMenuGrid")!;
+            const exitMenu = gui.getControlByName("ExitMenuGrid")!;
+            const mainMenuDefaultOptions = gui.getControlByName("StackPanel_MainMenu")!;
+            const chooseStoryMenu = gui.getControlByName("ChooseStoryGrid")!;
+            const createStoryMenu = gui.getControlByName("CreateStoryGrid")!;
+
+            // Setup the buttons in Main menu
+            const button_mainMenuChooseStoryButton = gui.getControlByName("Button_MainMenu_ChooseStory")! as Button;
+            const button_mainMenuCreateStoryButton = gui.getControlByName("Button_MainMenu_CreateStory")! as Button;
+            const button_mainMenuExitButton = gui.getControlByName("Button_MainMenu_Exit")! as Button;
+            const button_mainMenuExitConfirmButton = gui.getControlByName("Button_ConfirmExit")! as Button;
+            const button_mainMenuExitCancelButton = gui.getControlByName("Button_CancelExit")! as Button;
+
+            const openMainMenu = function(){
+                mainMenu.isVisible = true;
+                exitMenu.isVisible = false;
+                mainMenuDefaultOptions.isVisible = true;
+                chooseStoryMenu.isVisible = false;
+                createStoryMenu.isVisible = false;
+            }
+
+            const openExitMenu = function(){
+                mainMenu.isVisible = true;
+                exitMenu.isVisible = true;
+                mainMenuDefaultOptions.isVisible = false;
+                chooseStoryMenu.isVisible = false;
+                createStoryMenu.isVisible = false;
+            }
         
-        const mainMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI");
+            const openChooseStoryMenu = function(){
+                mainMenu.isVisible = false;
+                chooseStoryMenu.isVisible = true;
+                createStoryMenu.isVisible = false;
+            }
+        
+            const openCreateStoryMenu = function(){
+                mainMenu.isVisible = false;
+                chooseStoryMenu.isVisible = false;
+                createStoryMenu.isVisible = true;
+            }
+
+            const quit = function() {
+                window.close();
+            }
+            
+            button_mainMenuChooseStoryButton.onPointerUpObservable.add(openChooseStoryMenu);
+            button_mainMenuCreateStoryButton.onPointerUpObservable.add(openCreateStoryMenu);
+            button_mainMenuExitButton.onPointerUpObservable.add(openExitMenu);
+            button_mainMenuExitConfirmButton.onPointerUpObservable.add(quit);
+            button_mainMenuExitCancelButton.onPointerUpObservable.add(openMainMenu);
+
+            // SECTION 2: CHOOSE STORY MENU
+
+            // Setup the buttons in the Choose Story menu
+            const button_closeMenuChooseStory = gui.getControlByName("Button_Close_ChooseStory")!;
+            button_closeMenuChooseStory.onPointerUpObservable.add(openMainMenu);
+
+            // SECTION 3: CREATE STORY MENU
+
+            // Setup the buttons in the Create Story menu
+            const button_closeMenuCreateStory = gui.getControlByName("Button_Close_CreateStory")!;
+            button_closeMenuCreateStory.onPointerUpObservable.add(openMainMenu);
+
+            // Setup text input for create story menu
+            const inputTextContainer = gui.getControlByName("CreateStoryGrid")! as Grid;
+            const inputTextArea = new InputTextArea('Example InputTextArea', "Create a story");
+            inputTextArea.width = "80%";
+            inputTextArea.height = "100%";
+            inputTextArea.color = "white";
+            inputTextContainer.addControl(inputTextArea, 1, 0);
+            
+        });
+
+
+        /*
+        const mainMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
+        let loadedGUI = await  mainMenu.parseFromSnippetAsync("#HHCQ02#36");
+        
         this.gui = mainMenu;
-        
+
         // Adding the background image to the mainMenu
         const backgroundImage = new Image("backgroundImage", menuBackgroundUrl);
         backgroundImage.width = 1;  // Relative width, covering the full width of the GUI
@@ -76,6 +164,7 @@ export class StartScreen implements SceneClass {
         createButton("exit", "Exit", "50px", function() {
             window.close();
         });
+        */
 
         return scene;
     };
