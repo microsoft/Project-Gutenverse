ae29c4a282131a846443febd2a82be651f8be5b5
Michael Smith <mismit@microsoft.com>
2023-09-14 00:08:51 -0700

    Add in the scene/chapter text from segmentation into a 'speech' node as the narrator

--- server/pipeline/scenecompilationstage.py
+++ server/pipeline/scenecompilationstage.py
@@ -28,6 +28,7 @@ class SceneCompilationStage(Stage):
                 logger.info(f"{self} step found to be already completed for " + subfolder)
                 continue
             
+            segmentation_filepath = os.path.join(story_folder, subfolder, "scene.json")
             charactergen_filepath = os.path.join(story_folder, subfolder, "2_charactergen_stage.json")
             composition_filepath = os.path.join(story_folder, subfolder, "4_composition_stage.json")
             skybox_filepath = f"/stories/{str(context.id)}/{subfolder}/assets/skybox_360.png"
@@ -40,12 +41,17 @@ class SceneCompilationStage(Stage):
                 logger.info(f"{self} no composition output found for " + subfolder + " skipping scene compilation")
                 continue
 
+            segmentation_data = self.load_json_data(segmentation_filepath)
             charactergen_data = self.load_json_data(charactergen_filepath, "characters")
             composition_data = self.load_json_data(composition_filepath, "characters")
             # add image from character gen object
             for character_name, character_info in charactergen_data.items():
                 composition_data[character_name]['imageUrl'] = f"/stories/{str(context.id)}/{subfolder}/assets/{character_info.get('image')}"
 
+            # set scene story text under 'narrator' character for now
+            # since it is not broken down by speaker / not every story will have dialog
+            # exclusively from characters
+            composition_data["content"] = segmentation_data.get("content")
             scene_compilation_data = self.transform_composition_data(composition_data)
             
             scene_compilation_data["environment"] = skybox_filepath
@@ -67,26 +73,48 @@ class SceneCompilationStage(Stage):
         transformed_characters = []
 
         for character_name, character_info in composition_data.items():
+            # Check if character_info is not None
+            if character_info is None:
+                continue
+            
             transformed_character = {
                 "name": character_name,
-                "distanceFromCamera": 60,  # Default value
-                "angularDistanceFromCamera": character_info.get("distance_from_the_center_in_degree", 0),
-                "width": character_info.get("scale_x", 0),
-                "height": character_info.get("scale_y", 0),
-                "distanceFromGround": character_info.get("distance_from_the_floor", 0),
-                "imageUrl": character_info.get("imageUrl")
+                "distanceFromCamera": 60  # Default value
             }
+            
+            # Conditionally assign values if they exist in character_info
+            if "distance_from_the_center_in_degree" in character_info:
+                transformed_character["angularDistanceFromCamera"] = character_info["distance_from_the_center_in_degree"]
+            if "scale_x" in character_info:
+                transformed_character["width"] = character_info["scale_x"]
+            if "scale_y" in character_info:
+                transformed_character["height"] = character_info["scale_y"]
+            if "distance_from_the_floor" in character_info:
+                transformed_character["distanceFromGround"] = character_info["distance_from_the_floor"]
+            if "imageUrl" in character_info:
+                transformed_character["imageUrl"] = character_info["imageUrl"]
+
             transformed_characters.append(transformed_character)
 
-        transformed_data = {"characters": transformed_characters}
+        transformed_data = {
+            "characters": transformed_characters,
+            "speech": [
+                {
+                    "character": "narrator",
+                    "text": composition_data["content"]
+                }
+            ]            
+        }
         return transformed_data
- 
 
-    def load_json_data(self, filepath, rootNodeName):
+    def load_json_data(self, filepath, rootNodeName = None):
         if not os.path.isfile(filepath):
             logger.error(f"{self} file not found at " + filepath)
 
         with open(filepath, 'r') as file:
                 data = json.load(file)
-                json_data = data.get(rootNodeName, {})
+                if rootNodeName is not None:
+                    json_data = data.get(rootNodeName, {})
+                else:
+                    json_data = data
                 return json_data
