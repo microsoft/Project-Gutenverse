8837db75ce5454434bc28069c59baeaf1f177fda
Mike Chen <mikchen@microsoft.com>
2023-09-14 10:57:34 -0700

    temporarily use cpu for audio gen to avoid out of memory issues

--- server/pipeline/audiostage.py
+++ server/pipeline/audiostage.py
@@ -18,7 +18,7 @@ class AudioStage(Stage):
     def __init__(self, _music_duration=5, _audio_duration=2):
         start = time.time()
         # Load the Music Gen model with the CPU if we are not using the GPU, otherwise let Music Gen determine how to load the model
-        self.musicgen_model = MusicGen.get_pretrained('facebook/musicgen-small', device="cpu" if not config.UseGpu else None)
+        self.musicgen_model = MusicGen.get_pretrained('facebook/musicgen-small', device="cpu" if not config.UseGpuAudioGen else None)
         end = time.time()
         logger.info(f"MusicGen Model took {end-start} seconds")
 
@@ -26,7 +26,7 @@ class AudioStage(Stage):
 
         start = time.time()
         # Load the Audio Gen model with the CPU if we are not using the GPU, otherwise let Audio Gen determine how to load the model
-        self.audiogen_model = AudioGen.get_pretrained('facebook/audiogen-medium', device="cpu" if not config.UseGpu else None)
+        self.audiogen_model = AudioGen.get_pretrained('facebook/audiogen-medium', device="cpu" if not config.UseGpuAudioGen else None)
         end = time.time()
         logger.info(f"AudioGen Model took {end-start} seconds")
 
