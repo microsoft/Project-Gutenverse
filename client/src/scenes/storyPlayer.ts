a2ccdd07fe051c3e3407b70e494312a06d4fbee1
Michael Smith <mismit@microsoft.com>
2023-09-14 17:19:20 -0700

    URL fixes

--- client/src/scenes/storyPlayer.ts
+++ client/src/scenes/storyPlayer.ts
@@ -29,9 +29,9 @@ export class StoryPlayer {
             }, {loop: true});
         }
 
-        if (this.storyContent.scenes[this.currentSceneIndex].tts) {
-            console.log('loading text to speech from url', "http://localhost:5000" + this.storyContent.scenes[this.currentSceneIndex].tts);
-            this.currSceneSound = new Sound("backgroundMusic", "http://localhost:5000" + this.storyContent.scenes[this.currentSceneIndex].tts, this.sceneArgs.scene, () => {
+        if (this.storyContent.scenes[this.currentSceneIndex]["speech"][0].tts) {
+            console.log('loading text to speech from url', "http://localhost:5000" + this.storyContent.scenes[this.currentSceneIndex]["speech"][0].tts);
+            this.currSceneSound = new Sound("backgroundMusic", "http://localhost:5000" + this.storyContent.scenes[this.currentSceneIndex]["speech"][0].tts, this.sceneArgs.scene, () => {
                 this.currSceneSound!.play();
                 this.currSceneSound!.loop = false;
                 this.player.updateSound(this.currSceneSound!);
