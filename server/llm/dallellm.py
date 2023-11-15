202707d9da7a31694e44d7af009c7e301afe9562
Michael Smith <mike@mikelikescoding.com>
2023-11-15 00:53:03 -0800

    Dalle 3, GPT 4 turbo and OpenAI version bump changes

--- server/llm/dallellm.py
+++ server/llm/dallellm.py
@@ -5,6 +5,8 @@ from config import config
 from PIL import Image
 import openai
 import requests
+from openai import OpenAI
+
 
 class DalleLLM:
     
@@ -12,13 +14,21 @@ class DalleLLM:
         openai.api_key = config.OpenAIApiKey
 
     def generate(self, prompt, negative_prompt = "", height = 512, width = 512):
-        size = f"{height}x{width}"
-        response = openai.Image.create(
+        client = OpenAI(api_key=config.OpenAIApiKey)
+        response = client.images.generate(
+            model="dall-e-3",
             prompt=prompt,
+            size="1024x1024",
+            quality="standard",
             n=1,
-            size=size,
-            response_format="url"
         )
 
-        url = response["data"][0]["url"]
+        # response = openai.Image.create(
+        #     prompt=prompt,
+        #     n=1,
+        #     size=size,
+        #     response_format="url"
+        # )
+
+        url = response.data[0].url
         return Image.open(requests.get(url, stream=True).raw)
--- server/llm/openaillm.py
+++ server/llm/openaillm.py
@@ -3,6 +3,8 @@
 
 import os
 import openai
+
+from openai import OpenAI
 import json
 
 from config import config
@@ -29,20 +31,24 @@ class OpenAiLLM:
         
     def analize_scene(self, scene) -> str:
         analyzer_prompt = self.analysis_prompt.replace("{{story_data}}", scene.body)
-        output = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo-16k",
-                messages=[{
-                    "role": "user",
-                    "content": analyzer_prompt
-                }]
-            )
+
+        client = OpenAI(api_key=config.OpenAIApiKey)
+        output = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        response_format={ "type": "json_object" },
+        messages=[
+            {"role": "user", "content": analyzer_prompt},
+        ])
+
         print(output)
-        return output["choices"][0]["message"]["content"]
+        return output.choices[0].message.content
 
     def segment_block_into_scenes(self, story_data: str) -> dict:
         prompt = self.segmentation_prompt.replace("{{story_data}}", story_data)
         output = self.prompt(prompt)
-        jsonResponse = output["choices"][0]["message"]["content"]
+        jsonResponse = output.choices[0].message.content
+        if (jsonResponse.startswith('json')):
+            jsonResponse = jsonResponse.strip('json')
         try:
             json_data = json.loads(jsonResponse)
         except json.JSONDecodeError as e:
@@ -59,25 +65,26 @@ class OpenAiLLM:
     def get_composition(self, story_content, characters) -> str:
         composition_prompt = self.compositon_prompt.replace("{{story_content}}", story_content)
         composition_prompt = self.compositon_prompt.replace("{{characters}}", characters)
-        output = openai.ChatCompletion.create(
-                model = "gpt-3.5-turbo-16k",
-                messages=[{
-                    "role": "user",
-                    "content": composition_prompt
-                }],
-                temperature=0.1,
-            )
+        
+        client = OpenAI(api_key=config.OpenAIApiKey)
+        output = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        response_format={ "type": "json_object" },
+        messages=[
+            {"role": "user", "content": composition_prompt},
+        ])
         print(output)
-        return output["choices"][0]["message"]["content"]
+        return output.choices[0].message.content
     
     def prompt(self, prompt: str):
-        return openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo-16k",
-            messages=[{
-                "role": "user",
-                "content": prompt
-            }],
-            temperature=0.3,
-        )
+        client = OpenAI(api_key=config.OpenAIApiKey)
+        output = client.chat.completions.create(
+            response_format={ "type": "json_object" },
+            model="gpt-4-1106-preview",
+    
+            messages=[
+                {"role": "user", "content": prompt},
+            ])
+        return output
 
 llm = OpenAiLLM()
